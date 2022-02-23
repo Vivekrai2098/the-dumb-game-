f0a1989b2559801e8fbd1d9638e81ab14f16928b
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-02-23 21:44:28 +0530

    try and figure out the bug

--- d.js
+++ d.js
@@ -1,3 +1,3 @@
 function shyam(){
-	window.open("https://vivekrai2098.github.io/linkedwork/","","position=absolute,left=500px,top=230,width=400px,height=300px")
+	window.open("https://www.google.com/","","position=absolute,left=500px,top=230,width=400px,height=300px")
 }
