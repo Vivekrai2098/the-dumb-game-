766db6aff3bbcf09b7731d42eb5b723c20fcd703
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-02-23 21:42:26 +0530

    update new

--- d.js
+++ d.js
@@ -1,3 +1,3 @@
 function shyam(){
-	window.open("file:///home/kali/Desktop/dumb%20folder%20/d.html","","position=absolute,left=500px,top=230,width=400px,height=300px")
+	window.open("https://vivekrai2098.github.io/linkedwork/","","position=absolute,left=500px,top=230,width=400px,height=300px")
 }
